f4841d707ac448ba58071f9d1e5ed2851789f16d
etmjr03 <etmjr03@gmail.com>
2023-01-03 19:11:11 -0300

    Entendendo conceitos sobre variaveis

---
+++ variaveis.js
@@ -0,0 +1,39 @@
+/*
+    # Variáveis
+        * Nomes simbólicos para receber algum valor
+        * Atalhos de códigos
+        * Identificadores
+        *  3 palavras reservadas para criar uma variável:
+            * var
+            * let
+            * const
+*/
+
+
+// alterando valor da variável igual exemplo do armário.
+var clima = "frio";
+clima = "quente";
+
+console.log(clima)
+
+let nome = "Juninho";
+nome = "Edilson";
+
+console.log(nome)
+
+const numero = 10;
+// numero = 11;
+
+console.log(numero)
+
+// printando tipo de varável
+
+let teste = 10;
+let teste1 = "Juninho";
+let teste2 = false;
+let teste3 = {};
+
+console.log(typeof teste);
+console.log(typeof teste1);
+console.log(typeof teste2);
+console.log(typeof teste3);
