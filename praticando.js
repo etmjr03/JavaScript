dac21fba06f7a03df94ae77422e0727cb5ccf1b6
etmjr03 <etmjr03@gmail.com>
2023-01-04 10:38:39 -0300

    Interpolando e concatenando variaveis e textos

---
+++ praticando.js
@@ -0,0 +1,16 @@
+var name = "juninho";
+
+// agrupamento de declarações
+let age, isHuman
+
+age = 23;
+isHuman = true;
+
+console.log(name, age, isHuman)
+
+// interpolação, escrita de texto + variáveis
+// concatenando valores
+console.log("o " + name + " tem " + age + " anos")
+
+//interpolando valores com template literals or templates strings
+console.log(`o ${name} tem ${age} anos`)
