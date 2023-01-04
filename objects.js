c71f05fdc0767d5b7053d64ff46318bf5ac3f0e1
etmjr03 <etmjr03@gmail.com>
2023-01-04 10:45:44 -0300

    Praticando conceitos sobre object

---
+++ objects.js
@@ -0,0 +1,21 @@
+// Object name é propriedade e "juninho" é valor
+
+const user = {
+    name: "juninho",
+    age: 23,
+    isAdmin: true
+}
+
+console.log(`${user.name} tem ${user.age} anos e é um admin? ${user.isAdmin}`)
+
+const dado = {
+    lados: 6,
+    lado1: 1,
+    lado2: 2,
+    lado3: 3,
+    lado4: 4,
+    lado5: 5,
+    lado6: 6
+}
+
+console.log("quantos lados tem um dado?", dado.lados)
