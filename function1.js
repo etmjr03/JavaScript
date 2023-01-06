c1f33e6cc44923670ff506de42e7d76826e048f1
etmjr03 <etmjr03@gmail.com>
2023-01-06 14:01:43 -0300

    Conceitos sobre parametro e argumento em functions

---
+++ function1.js
@@ -0,0 +1,8 @@
+//função anonima ou function expression, espressão
+
+// number1 e number2 são parâmetros
+const sum = function(number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(2, 3) // argumentos
