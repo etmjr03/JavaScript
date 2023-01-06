e57838cbc6ec392756a89be85ccc83c5fe03bf6f
etmjr03 <etmjr03@gmail.com>
2023-01-06 16:24:04 -0300

    Entendo alguns conceitos de funcao

---
+++ function2.js
@@ -0,0 +1,7 @@
+function FazerWhey(ingrediente1, ingrediente2) {
+    return ingrediente1 + " e " + ingrediente2;
+}
+
+const copo = FazerWhey('Whey', 'água')
+
+console.log(`o liquidificador bateu os seguintes produtos: ${copo}`)
