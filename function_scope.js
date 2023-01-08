017011ef21e61c852de509b7cc1efa693f7c3a65
etmjr03 <etmjr03@gmail.com>
2023-01-07 22:33:12 -0300

    Entendendo conceitos sobre scope da funcao

---
+++ function_scope.js
@@ -0,0 +1,11 @@
+// Function Scope
+
+let subjetc = 'create video';
+
+function createThink(subjetc) {
+    subjetc = 'working';
+    return subjetc;
+}
+
+console.log(createThink(subjetc));
+console.log(subjetc);
